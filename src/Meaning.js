6cd265d053e96e6806ffcb34b44f8d9b94342582
oszka <oszka@protonmail.com>
2022-04-08 15:29:53 +0100

    css styling

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,10 +1,10 @@
 import React from "react";
-import "./Meaning.css";
+
 import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
-    <div className="Meanings">
+    <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
--- src/Search.js
+++ src/Search.js
@@ -47,6 +47,7 @@ export default function Search() {
             />
           </div>
         </div>
+
         <Results results={results} />
       </div>
 
