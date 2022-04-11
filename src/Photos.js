113f7dcbd2a6b85f077f27701efebbfaf326438e
oszka <oszka@protonmail.com>
2022-04-11 16:16:44 +0100

    adding rel

--- src/Photos.js
+++ src/Photos.js
@@ -11,7 +11,11 @@ export default function Photos(props) {
             {props.photos.map(function (photo, index) {
               return (
                 <div className="col-sm-4" key={index}>
-                  <a href={photo.src.original} target="_blank" rel="noopener">
+                  <a
+                    href={photo.src.original}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     <img
                       src={photo.src.landscape}
                       alt={photo.alt}
